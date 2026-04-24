1581d333be5474a9787ee07bac87a88f9e303939
betterwork7777 <kiontroy@gmail.com>
2026-04-23 17:51:06 -0700

    Create ChildSupport component for child support services

---
+++ src/app/child-support/page.tsx
@@ -0,0 +1,13 @@
+export default function ChildSupport() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Child Support Services</h1>
+
+      <h2>Request Child Support</h2>
+      <p>This will help you file for new child support.</p>
+
+      <h2>Modify Child Support</h2>
+      <p>This will help you request a modification of an existing order.</p>
+    </div>
+  );
+}
