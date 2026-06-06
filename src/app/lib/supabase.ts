aab5ea4bceaa9c149815f2d9f1edc20cb7ce5180
betterwork7777 <kiontroy@gmail.com>
2026-06-05 22:37:51 -0700

    Fix formatting in supabase.ts

--- src/app/lib/supabase.ts
+++ src/app/lib/supabase.ts
@@ -3,4 +3,4 @@ import { createClient } from "@supabase/supabase-js";
 export const supabase = createClient(
   "https://brjxclxlptwjzawevzwx.supabase.co",
   "sb_publishable_modA-En_NPTxFcUOHSjVdA_mzK2-Dpy"
-);;
+);
